86ca981219c9f1c5dcf5161520de1dae53305d63
astoroge <astoroge@gmail.com>
2022-09-16 15:14:38 +0800

    3

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ lastname = input('Введите фамилию: ')
 age = int(input('Введите возраст: '))
 kg = int(input('Введите вес: '))
 
-if age < 30 and 50 < kg < 120:
+if age < 30 and 50 <= kg < 120:
     print(f'{name} {lastname}, {age} лет, вес {kg} - хорошее состояние')
-if age > 30 and 50 > kg or kg > 120:
+
+
+if age > 30 and kg < 50 or kg > 120:
     print(f'{name} {lastname}, {age} лет, вес {kg} - следует заняться собой')
-if age > 40 and 50 > kg or kg > 120:
+
+
+elif age > 40 and kg < 50 or kg > 120:
     print(f'{name} {lastname}, {age} лет, вес {kg} - следует обратится к врачу!')
