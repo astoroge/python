242e4cf1b336fa843ab1606c897fd9c23dadbda2
astoroge <astoroge@gmail.com>
2022-09-19 10:36:09 +0800

    цикл for in

--- main.py
+++ main.py
@@ -1,49 +1,24 @@
-# empty_list = []
-# friends = ['Max','Leo','Kate']
-# print(type(friends))
-# print('Второй друг: ', friends[1])
-# print('Первый друг с концом: ', friends[-1])
-# print(friends[1:2])
-# print(friends[:2])
-# print(friends[1:])
-# print(len(friends))
-# friends.append('Ron')
-# print(friends)
-# print(friends.pop())
-# friends.remove('Max')
-# del friends[0]
-# print(friends)
-# friends.clear()
-# print(friends)
+friend_name = 'Max'
+friends = ['Max', 'Leo', 'Kate']
+roles = ('admin', 'guest', 'user')
 
-# hero = 'Superman'
-# if hero.find('man') != -1: # не равно минус один, значит если есть; равно минус один тоже самое что нету
-#     print('Есть')
-# if 'man' in hero:
-#     print('Есть')
+if 'Max' in friends:
+    print('У меня есть этот друг')
 
-# goals = ['стать гуру языка python', 'здоровье', 'накормить кота']
-# if 'здоровье' in goals:
-#     print('Все хорошо')
+if 'M' in friend_name:
+    print('Буква М есть в имени друга')
 
-# Кортеж - не изменяемый список
+i = 0
+while i < len(friends):
+    friend = friends[i]
+    print(friend)
+    i += 1
 
-from itertools import count
-from unittest import result
+for friend in friends:
+    print(friend)
 
-print('СОРЕВНОВАНИЯ ПО PYTHON')
-count = int(input('Введите количество участников: '))
-i = count
-members = []
-while i > 0:
-    name = input('Кто занял {} место: '.format(i))
-    members.append(name)
-    i -= 1 # i = i - 1
-print('В соревновании участвовали: ', members)
+for letter in friend_name:
+    print(letter)
 
-members.reverse()
-
-result = members[:3]
-
-result = 'Победители: {}. Поздравляем!'.format(result)
-print(result)
+for role in roles:
+    print(role)
