4891a25c24c33e7986251eb2353bbcff3176b230
astoroge <astoroge@gmail.com>
2022-10-04 15:45:22 +0800

    Работа с файлами

--- get_list.py
+++
@@ -1,6 +0,0 @@
-import random
-
-def get_random(list):
-    if list:
-        result = random.choice(list)
-        return result
--- main.py
+++
@@ -1,8 +0,0 @@
-from get_dir import remove_dir
-import get_dir
-import get_list
-
-get_dir.create_dir()
-remove_dir()
-
-print(get_list.get_random([1, 3, 6, 7]))
