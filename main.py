16703025ed8895c329d9c41862c472cd93768c6c
astoroge <astoroge@gmail.com>
2022-10-05 11:06:14 +0800

    Тернарный оператор и исключительные ситуаций

--- main.py
+++ main.py
@@ -1,33 +1,34 @@
 #############################################
-# Тернарный оператор
-# is_russian = False
-# print('Привет' if is_russian else 'Hello')
+# Тернарный оператор                        #
+# is_russian = False                        #
+# print('Привет' if is_russian else 'Hello')#
 #############################################
-# Обработка исключительных ситуаций(ошибок)
-# number = int(input('Введите число: '))
-# try:
-#     result = 100 / number
-# except:
-#     print('Ошибка. Деление на 0.')
-# print('Конец')
-#
-# Обработка конкретных исключительных ситуаций(ошибок)
-# number = int(input('Введите число: '))
-# try:
-#     result = 100 / number
-# except ZeroDivisionError:
-#     print('Попытка деления на 0.')
-# except Exception:
-#     print('Неизвестная ошибка.')
-#
-# Чтобы посмотреть информацию об исключении
-# number = int(input('Введите число: '))
-# try:
-#     result = 100 / number
-# except ZeroDivisionError as e:
-#     print('Попытка деления на 0.')
-#     print('Информация об исключении', e)
-# except Exception as e:
-#     print('Неизвестная ошибка.')
-#     print('Информация об исключении', e)
+# Обработка исключительных ситуаций(ошибок) #
+# number = int(input('Введите число: '))    #
+# try:                                      #
+#     result = 100 / number                 #
+# except:                                   #
+#     print('Ошибка. Деление на 0.')        #
+# print('Конец')                            #
+#                                           #
+# Обработка конкретных исключительных       #
+# ситуаций(ошибок)                          #
+# number = int(input('Введите число: '))    #
+# try:                                      #
+#     result = 100 / number                 #
+# except ZeroDivisionError:                 #
+#     print('Попытка деления на 0.')        #
+# except Exception:                         #
+#     print('Неизвестная ошибка.')          #
+#                                           #
+# Чтобы посмотреть информацию об исключении #
+# number = int(input('Введите число: '))    #
+# try:                                      #
+#     result = 100 / number                 #
+# except ZeroDivisionError as e:            #
+#     print('Попытка деления на 0.')        #
+#     print('Информация об исключении', e)  #
+# except Exception as e:                    #
+#     print('Неизвестная ошибка.')          #
+#     print('Информация об исключении', e)  #
 #############################################
